d5b161ec703a42005c8323c5d49ec72ad7efc6af
Tony Kuriakose <t4tonykuriakose@gmail.com>
2026-09-09 07:51:35 +0530

    fix:login page bug

---
+++ fix.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+prisma.user.update({ where: { email: 't4tonykuriakose@gmail.com' }, data: { isActive: true } })
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
